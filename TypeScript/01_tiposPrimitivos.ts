71f98411ee77b270f633055715c8c43ab46fb948
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2024-01-03 20:50:12 -0300

    Aula 01) Tipos Primitivos

---
+++ TypeScript/01_tiposPrimitivos.ts
@@ -0,0 +1,22 @@
+/* Declarando variáveis */
+// -> Tipo explícito
+let myExplicitString: string = "I'm a string!";
+
+// -> Tipo implícito
+let myImplicitString = "I'm too!";
+
+/* Tipos Primitivos */
+// -> Simples
+let myString: string = "";
+let myNumberInt: number = 1;
+let myNumberFloat: number = 1.1;
+let myBoolean: boolean = true;
+
+// Outros
+let myAny: any = ""; // Pode alternar de tipo
+let myUnknown: unknown = ""; // ????????
+let myNever: never; // NUNCA vai ter um valor
+
+// JavaScript like
+let myNull: null = null;
+let myUndefined: undefined = undefined;
