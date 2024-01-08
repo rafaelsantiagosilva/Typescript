0862d02257d6af9f5940f43f2f9bec8b3f239cee
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2024-01-08 11:45:13 -0300

    Aula 03) Tuplas

---
+++ JavaScript/03_tuplas.js
@@ -0,0 +1,5 @@
+// Tuplas (em TS) são arrays com tipos definidos por indice
+var ourTuple; // Tem que ser obrigatoriamente nessa ordem
+ourTuple = [5, false, 'Codando...'];
+// Tuplas readonly
+var ourTupleReadOnly = [1, true, 'verdadeiro']; // Não pode alterar ou adicionar
---
+++ TypeScript/03_tuplas.ts
@@ -0,0 +1,7 @@
+// Tuplas (em TS) são arrays com tipos definidos por indice
+let ourTuple: [number, boolean, string]; // Tem que ser obrigatoriamente nessa ordem
+ourTuple = [5, false, 'Codando...']; 
+
+// Tuplas readonly
+let ourTupleReadOnly: [number, boolean, string] = [1, true, 'verdadeiro']; // Não pode alterar ou adicionar
+
