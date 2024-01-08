64e13e737329afea90dc24b6dcd059b8ca5dbd2a
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2024-01-08 13:47:37 -0300

    Configurando Gitignore

--- JavaScript/06_aliases.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var CAR_YEAR = 2001;
-var CAR_TYPE = "Toyota";
-var CAR_MODEL = "Corolla";
-var MY_CAR = {
-    year: CAR_YEAR,
-    type: CAR_TYPE,
-    model: CAR_MODEL,
-};
--- JavaScript/07_interfaces.js
+++
@@ -1,9 +0,0 @@
-var rectangle = {
-    height: 20,
-    width: 10
-};
-var coloredRectangle = {
-    height: 20,
-    width: 10,
-    color: "red"
-};
